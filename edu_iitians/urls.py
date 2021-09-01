3d8279d19a2c15e0248a4b4aa1a94f31caf2ffd6
rajilpatel <rajilpatel001@gmail.com>
2021-09-01 14:15:23 +0530

    url pattern in urls.py

--- edu_iitians/urls.py
+++ edu_iitians/urls.py
@@ -30,6 +30,13 @@ urlpatterns = [
  
 ]
 
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+urlpatterns = [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
