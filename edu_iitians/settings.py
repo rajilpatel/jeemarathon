21e95b722278896106391a823d09d8cf937352d5
rajilpatel <rajilpatel001@gmail.com>
2021-09-01 11:02:37 +0530

    added database settings

--- edu_iitians/settings.py
+++ edu_iitians/settings.py
@@ -84,22 +84,23 @@ WSGI_APPLICATION = 'edu_iitians.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'eduiitianabcd123',
-#         'USER': 'eduiitianuserabcd123',
-#         'PASSWORD': '91247quX129',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jeemarathon',
+        'USER': 'jeemarathonuser',
+        'PASSWORD': 'edujeemara',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
